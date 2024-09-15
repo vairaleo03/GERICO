8d5c660b03d375e985d1cb12f48a8778743a357d
vairaleo03 <149255909+vairaleo03@users.noreply.github.com>
2024-09-15 22:58:21 +0200

    Add files via upload

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -4,7 +4,7 @@ import { io } from 'socket.io-client';
 
 export const SocketContext = createContext();
 
-const socket = io(process.env.REACT_APP_API_URL ||'https://api.render.com/deploy/srv-crjedq2j1k6c73flonc0?key=zRmNPSvH9Rs/api', {
+const socket = io(process.env.REACT_APP_API_URL ||'http://localhost:5000', {
   withCredentials: true,
 });
 
