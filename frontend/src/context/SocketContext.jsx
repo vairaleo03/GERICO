fafe55227ccbd7848d28d090b116b6ee72cec949
vairaleo03 <149255909+vairaleo03@users.noreply.github.com>
2024-09-16 18:38:01 +0200

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -1,10 +1,14 @@
-// src/context/SocketContext.js
 import React, { createContext } from 'react';
 import { io } from 'socket.io-client';
 
 export const SocketContext = createContext();
 
-const socket = io('https://gerico.onrender.com', {
+// Verifica se siamo in ambiente di produzione o sviluppo
+const socketURL = process.env.NODE_ENV === 'production'
+  ? 'https://gerico.onrender.com' // URL di produzione
+  : 'http://localhost:5000'; // URL di sviluppo
+
+const socket = io(socketURL, {
   withCredentials: true,
 });
 
