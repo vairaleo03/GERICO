a64af0c8d34815918f3bd8ce3d77a7b52b3528c3
vairaleo03 <149255909+vairaleo03@users.noreply.github.com>
2024-09-16 18:37:30 +0200

    Update api.jsx

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,8 +1,12 @@
-// src/services/api.js
 import axios from 'axios';
 
+// Verifica se siamo in ambiente di produzione o sviluppo
+const baseURL = process.env.NODE_ENV === 'production'
+  ? 'https://gerico.onrender.com/api' // URL del backend in produzione
+  : 'http://localhost:5000/api'; // URL del backend in sviluppo
+
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
+  baseURL,
   withCredentials: true, // Deve essere true per inviare i cookie
 });
 
